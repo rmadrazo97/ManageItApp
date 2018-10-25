0185daa98ce142d27ef775007bd9d9a18d6054e5
rmadrazo97 <jmadrazo7@gmail.com>
2018-10-25 17:49:22 -0600

    Unzip Akounting

---
+++ config/version.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    'name'      =>  'Akaunting',
+
+    'code'      =>  'Recurring',
+
+    'major'     =>  '1',
+
+    'minor'     =>  '2',
+
+    'patch'     =>  '16',
+
+    'build'     =>  '',
+
+    'status'    =>  'Stable',
+
+    'date'      =>  '14-September-2018',
+
+    'time'      =>  '12:30',
+
+    'zone'      =>  'GMT +3',
+
+];
